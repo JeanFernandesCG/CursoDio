478a295060e5cec93e84772b161eaa4b75dfd5a7
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-13 14:29:01 -0400

    condicional IF ELSE correto :D

--- aula2-Condicionais.js
+++ aula2-Condicionais.js
@@ -1,10 +1,7 @@
-const nmr = 0;
+const nmr = 22;
 
-const ePAR = nmr % 2 == "0";
+const ePAR = nmr % 2 === 0;
 
 if (ePAR) {
   console.log("é par essa budega ai");
-}
-if (!ePAR) {
-  console.log("é impar lascou");
-}
+} else console.log("lascou é impar");
