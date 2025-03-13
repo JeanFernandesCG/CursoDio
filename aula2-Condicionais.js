c4ed8e0920c654578651d7723d1910ec14016594
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-13 14:25:28 -0400

    condicional IF

---
+++ aula2-Condicionais.js
@@ -0,0 +1,10 @@
+const nmr = 22;
+
+const ePAR = nmr % 2 == "0";
+
+if (ePAR) {
+  console.log("é par essa budega ai");
+}
+if (!ePAR) {
+  console.log("é impar lascou");
+}
--- aula2-restoDivisão.js
+++
@@ -1,6 +0,0 @@
-const nmr = 22;
-
-const ePAR = nmr % 2 == "0"; //é possivel usar 2 '=' para comparar entre tipos
-// usando 3'=' ele ignora tipo e compara direto, ao usar é bom sempre considerar os tipos.
-
-console.log(ePAR);
