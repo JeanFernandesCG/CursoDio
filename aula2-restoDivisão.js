faf523ffcf2d7d6727c190b5f522892fb3af1e86
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-13 14:14:37 -0400

    atividade um da segunda aula

---
+++ aula2-restoDivisão.js
@@ -0,0 +1,7 @@
+const nmr = 27;
+//é uma condição booleana, ou é ou nao é
+const ePAR = nmr % 2 === 0; // o resto da divisão por 2 é comparado com zero para ver se ele é par
+//caso for ele retorna true, caso nao retorna false.
+//parentes na conta serve para a respeitar a ordem dos calculos.
+
+console.log(ePAR);
