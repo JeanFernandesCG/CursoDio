6605647a3536736db6e809ff76c5d60c8155acae
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-16 16:53:56 -0400

    função main

---
+++ aula2-organizacao.js
@@ -0,0 +1,12 @@
+function exemplo() {
+  console.log("isso é uma função sendo chamada pelo main");
+}
+
+function main() {
+  //uma funçao como outra  mas que abriga todas outras
+
+  console.log("hello world !!");
+  exemplo();
+}
+
+main();
