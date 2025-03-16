feabb198c0e0bb1e0b77f774854073a653aa8ebb
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-16 17:34:43 -0400

    verificacao de idade

---
+++ verificaIdade.js
@@ -0,0 +1,14 @@
+function verifica(idade) {
+  if (idade < 18) {
+    console.log("Toma Leite!");
+  } else {
+    console.log("Bora Bebeee!");
+  }
+}
+
+function main() {
+  idade = 22;
+  verifica(idade);
+}
+
+main();
