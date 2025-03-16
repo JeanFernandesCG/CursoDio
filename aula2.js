0b09e4369afeba494de376aa15485bb92d98e586
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-16 16:47:59 -0400

    aula 2 js funções

--- aula2.js
+++ aula2.js
@@ -5,8 +5,23 @@ function contador(numero) {
 
 contador("um");
 contador("dois");
-contador("tres");
-contador("quatro");
-contador("cinco");
 // é passado um parametro para função(numeros) e dentro da função executa o console.log
 //assim pra cada execução ele repete o mil e um e acrescenta o q foi passado .
+
+function quadrado(valor) {
+  return valor * valor; //é possivel retornar valores apos execução de um funçao
+}
+
+const quadradovalor = quadrado(10);
+console.log(quadradovalor); //assim sera impresso o resultado.
+console.log(quadradovalor + quadradovalor); // é possivel tmb realizar operações com esses resultados
+
+function add_juros(valor, percentalJuros) {
+  //criação de um função simples para acrescimo de juros.
+  const valorAcrescimo = (percentalJuros / 100) * valor;
+  return valor + valorAcrescimo;
+}
+
+console.log(add_juros(150, 10));
+console.log(add_juros(180, 15));
+console.log(add_juros(130, 20));
