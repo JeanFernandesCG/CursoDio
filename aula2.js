5af704b32032f3b948a8bf1202b777a35507c93f
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-16 16:32:21 -0400

    atualizando conceitos

--- aula2.js
+++ aula2.js
@@ -1,6 +1,12 @@
-function teste() {
+function contador(numero) {
   //"()" insira os parametros da função
-  console.log("isso é uma função!");
+  console.log("mil e " + numero); // com parametros passado é possivel :
 }
 
-teste();
+contador("um");
+contador("dois");
+contador("tres");
+contador("quatro");
+contador("cinco");
+// é passado um parametro para função(numeros) e dentro da função executa o console.log
+//assim pra cada execução ele repete o mil e um e acrescenta o q foi passado .
