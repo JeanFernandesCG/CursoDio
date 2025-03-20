2fde6be56e55910eb5509eedfcde64c78a2af227
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-20 15:36:31 -0400

    importação exemplos

---
+++ testeMain.js
@@ -0,0 +1,11 @@
+const funcoes = require("./importação");
+// maiores erros consite em exporta errado, ou-----------=-========================================================limporta errado
+
+console.log(funcoes);
+
+/*OBJECT DESTRUCTURING
+
+const { gets, print } = require("./importação");
+
+print(gets());
+ SE IMPORTA OS AQUIVOS DENTRO DO OUTRO ARQUIVO DIRETO*/
