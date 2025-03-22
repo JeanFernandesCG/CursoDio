e9442889cfaf7b90cf9b4af97063d2d1b322d911
JeeanGDZL <jeeanfrnnds@gmail.com>
2025-03-22 10:16:04 -0400

    +resolução dio

--- salarioTransferir.js
+++ salarioTransferir.js
@@ -5,7 +5,7 @@ O salario a ser transferido é calculado da seguinte forma:
 valor bruto do salario - porcentagem do imposto referente ao salario + beneficios;
 Para calcular o percentual de imposto segue as aliquotas:
 de 0 a 1100 - 5%
-de 1100 a 2500 - 10%
+de 1101 a 2500 - 10%
 mais que 2500 - 15%
  */
 
@@ -27,3 +27,25 @@ if (salarioBruto <= 1100) {
   const salarioLiq = desconto + beneficios;
   print("é transferido o salario liquido no valor de: " + salarioLiq);
 }
+/* RESOLUÇÃO DIO
+const valorSalario = gets();
+const valorBeneficios = gets();
+function calcularPorcentagem(salario, percentual) {
+  return valor * (percentual / 100);
+}
+
+function pegarAliquota(salario) {
+  if (salario >= 0 && salario <= 1100) {
+    return 5;
+  } else if (salario >= 1101 && salario <= 2500) {
+    return 10;
+  } else {
+    return 15;
+  }
+}
+
+const aliquotaImposto = pegarAliquota(valorSalario);
+const valorImposto = calcularPorcentagem(valorSalario, aliquotaImposto);
+const valorATransferir = valorSalario - valorImposto + valorBeneficios;
+print(valorATransferir);
+*/
